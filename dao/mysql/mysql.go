b16f64d5345406d620f07162af69459da817db12
charminghacker <2560428298@qq.com>
2024-02-20 18:28:44 +0800

    更改redis和mysql配置为单例模式

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -8,30 +8,34 @@ import (
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"sync"
 )
 
 var (
-	db *gorm.DB
+	db   *gorm.DB
+	once sync.Once
 )
 
 func Init(cfg *settings.MySQLConfig) (err error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
-		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
-	// 也可以使用MustConnect连接不成功就panic
-	db, err = gorm.Open(mysql.New(mysql.Config{
-		DSN: dsn, // DSN data source name
-	}), &gorm.Config{})
-	if err != nil {
-		zap.L().Error("connect DB failed, err:%v\n", zap.Error(err))
-		return
-	}
-	sqlDB, err := db.DB()
-	sqlDB.SetMaxIdleConns(cfg.MaxIdleConns)
-	sqlDB.SetMaxOpenConns(cfg.MaxOpenConns)
-	err = db.AutoMigrate(&models.User{}, &models.Community{}, &models.Post{}, &models.Follow{}, &models.Fan{}, &models.Message{})
-	if err != nil {
-		zap.L().Error("auto migrate tables failed", zap.Error(err))
-		return err
-	}
+	once.Do(func() {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+			cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
+		// 也可以使用MustConnect连接不成功就panic
+		db, err = gorm.Open(mysql.New(mysql.Config{
+			DSN: dsn, // DSN data source name
+		}), &gorm.Config{})
+		if err != nil {
+			zap.L().Error("connect DB failed, err:%v\n", zap.Error(err))
+			return
+		}
+		sqlDB, err := db.DB()
+		sqlDB.SetMaxIdleConns(cfg.MaxIdleConns)
+		sqlDB.SetMaxOpenConns(cfg.MaxOpenConns)
+		err = db.AutoMigrate(&models.User{}, &models.Community{}, &models.Post{}, &models.Follow{}, &models.Fan{}, &models.Message{})
+		if err != nil {
+			zap.L().Error("auto migrate tables failed", zap.Error(err))
+			return
+		}
+	})
 	return
 }
--- dao/redis/redis.go
+++ dao/redis/redis.go
@@ -4,24 +4,27 @@ import (
 	"SHforum_backend/settings"
 	"fmt"
 	"github.com/go-redis/redis"
+	"sync"
 )
 
 var (
 	client *redis.Client
+	once   sync.Once
 	Nil    = redis.Nil
 )
 
 func Init(cfg *settings.RedisConfig) (err error) {
-	client = redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%d",
-			cfg.Host,
-			cfg.Port,
-		),
-		Password: cfg.Password, //取不到默认为空
-		DB:       cfg.DB,       //默认为0
-		PoolSize: cfg.PoolSize,
+	once.Do(func() {
+		client = redis.NewClient(&redis.Options{
+			Addr: fmt.Sprintf("%s:%d",
+				cfg.Host,
+				cfg.Port,
+			),
+			Password: cfg.Password, //取不到默认为空
+			DB:       cfg.DB,       //默认为0
+			PoolSize: cfg.PoolSize,
+		})
 	})
-
 	_, err = client.Ping().Result()
 	return err
 }
